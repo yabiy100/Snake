9c932217fd9369beee42e9ae2792ee04e19dd169
yabiy100 <yasin.biyikli@hhu.de>
2020-04-09 17:17:35 +0200

    - ADD Possition class

---
+++ src/main/java/de/hhu/snake/Components/Possition.java
@@ -0,0 +1,40 @@
+package de.hhu.snake.Components;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Possition {
+    int X;
+    int Y;
+    final int STEP_SIZE = 20;
+
+    public void move(Direction direction) {
+        switch (direction) {
+            case NORTH:
+                Y -= STEP_SIZE;
+                break;
+            case EAST:
+                X += STEP_SIZE;
+                break;
+            case SOUTH:
+                Y += STEP_SIZE;
+                break;
+            case WEST:
+                X -= STEP_SIZE;
+                break;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Possition)) {
+            return false;
+        }
+        Possition toCompare = (Possition) o;
+        return X == toCompare.getX() && Y == toCompare.getY();
+    }
+}
