9c7addc7708e8083bc440f6136e07e67770a5801
yabiy100 <yasin.biyikli@hhu.de>
2020-04-13 00:59:38 +0200

    - ADD move method - ADD grow method

--- src/main/java/de/hhu/snake/Components/Tail.java
+++ src/main/java/de/hhu/snake/Components/Tail.java
@@ -1,12 +1,14 @@
 package de.hhu.snake.Components;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
 @Data
+@NoArgsConstructor
 public class Tail {
 
     List<Possition> possitions;
@@ -16,17 +18,28 @@ public class Tail {
         possitions.add(startingPossiting);
     }
 
-    public int[] getXPossitions(){
+    public int[] getXPossitions() {
         int[] XPossitions = new int[possitions.size()];
         IntStream.range(0, possitions.size())
-                .forEach(i -> XPossitions[i] = possitions.get(i).getX() );
+                .forEach(i -> XPossitions[i] = possitions.get(i).getX());
         return XPossitions;
     }
 
-    public int[] getYPossitions(){
+    public int[] getYPossitions() {
         int[] YPossitions = new int[possitions.size()];
         IntStream.range(0, possitions.size())
-                .forEach(i -> YPossitions[i] = possitions.get(i).getY() );
+                .forEach(i -> YPossitions[i] = possitions.get(i).getY());
         return YPossitions;
+    }
+
+    public void move(Possition possition) {
+        if (!possitions.isEmpty()) {
+            possitions.remove(possitions.size());
+        }
+        possitions.add(0, possition);
+    }
+
+    public void grow(Possition possition) {
+        possitions.add(0, possition.copy());
     }
 }
